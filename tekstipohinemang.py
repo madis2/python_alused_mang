3754ac7e546cbb85377bc3cc87d06ca43bf7d98d
matis <98737015+madis2@users.noreply.github.com>
2022-03-24 17:17:14 +0200

    Update tekstipohinemang.py

--- tekstipohinemang.py
+++ tekstipohinemang.py
@@ -11,25 +11,85 @@ health = 100
 kuld = 10
 attack = 1
 defense = 1
-relv = None
+relv = 1
 
-peasant_health = 50
-peasant_kuld = 8
-peasant_attack = 1
-peasant_defense = 1
+def peasant():
+    peasant_health = 50
+    peasant_kuld = 8
+    peasant_attack = 1
+    peasant_defense = 1
+
+mook_dmg = random.randint(5,15)
 
 print('Kohtad kurja talupoega kes tahab rünnata sind.')
 
 def valikud():
-    print('[1] Ründa vastast')
-    print('[2] Ravi end')
+    print('[1] Ründa vastast.')
+    print('[2] Bloki (30% tõenäosus). ')
+    print('[3] Ravi end 10 elu võrra.')
     
     valik = int(input('Vali enda tegevus: '))
     return valik
 
-valik = valikud()
-
-while peasant_health >= 0:
-    
-    if option == 1:
-            
+def kaklus():
+    while peasant_health >= 0:
+        valik = valikud()
+        
+        dmg = random.randint(5,15) # Kahju mis teed vastasele
+        peasant_dmg = random.randint(1,10) # Kahju mis tehakse sulle 
+        protsent = 30
+        
+        print('─────────────────────────────────────')
+        
+        if valik == 1:
+            if relv == None:
+                if dmg >= 12:
+                    peasant_health -= dmg
+                    health -= peasant_dmg
+                    print(f'Kriitiline löök. Tegid {dmg} kahju')
+                    print(f'Vastane tegi sulle {peasant_dmg} kahju.')
+                    print(f'Sinu elud: {health}')
+                    print(f'Vastase elud: {peasant_health}')
+                    print('─────────────────────────────────────')
+                elif dmg in range(7,11):
+                    peasant_health -= dmg
+                    health -= peasant_dmg
+                    print(f'Hea löök. Tegid {dmg} kahju')
+                    print(f'Vastane tegi sulle {peasant_dmg} kahju.')
+                    print(f'Sinu elud: {health}')
+                    print(f'Vastase elud: {peasant_health}')
+                    print('─────────────────────────────────────')
+                else:
+                    peasant_health -= dmg
+                    health -= peasant_dmg
+                    print(f'Halb löök. Tegid {dmg} kahju')
+                    print(f'Vastane tegi sulle {peasant_dmg} kahju.')
+                    print(f'Sinu elud: {health}')
+                    print(f'Vastase elud: {peasant_health}')
+                    print('─────────────────────────────────────')
+            else:
+                peasant_health -= mook_dmg
+                health -= peasant_dmg
+                print(f'Tegid mõõgaga {mook_dmg} kahju.')
+                print(f'Vastane tegi sulle {peasant_dmg} kahju.')
+                print(f'Sinu elud: {health}')
+                print(f'Vastase elud: {peasant_health}')
+                print('─────────────────────────────────────')
+        elif valik == 2:
+            if random.randint(0,100) > protsent:
+                health -= peasant_dmg
+                print(f'Vastane tegi sulle {peasant_dmg}.')
+                print(f'Sinu elud: {health}')
+                print('─────────────────────────────────────')
+            else:
+                blokk = random.randint(0,3)
+                peasant_health -= blokk
+                print(f'Blokeerides löögi tegid vastasele {blokk} kahju.')
+                print('─────────────────────────────────────')
+        else:
+            health += 10
+            peasant_dmg = random.randint(1,10) # Kahju mis tehakse sulle
+            health -= peasant_dmg
+            print(f'Vastane tegi sulle {peasant_dmg} kahju.')
+            print(f'Sinu elud: {health}')
+            print('─────────────────────────────────────')
