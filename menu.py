6a1d33cbd474cb39cecc2653a6f65e605becffcf
NiilsR <98737083+NiilsR@users.noreply.github.com>
2022-03-30 21:03:42 +0300

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,25 @@
+import time
+import random
+print()
+print("--------------------")
+print("Kalevipoja otsingud")
+print("--------------------")
+print()
+print()
+print("Credits: Roger Niils, Matis Russi")
+print()
+print("Versioon: 0.5")
+print()
+print()
+algus = int(input("1. Start 2. Exit  "))
+print()
+if algus == 2:
+    exit()
+    
+elif algus == 1:
+    user_name = input("Mis on teie karakteri nimi?  ")
+elif algus == 2:
+    exit()
+
+time.sleep(1)
+print("Siin on sinu seikluse algus")
