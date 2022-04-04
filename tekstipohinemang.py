deea63b7c821e9ad8dd3e3c6679e0e1196334817
matis <98737015+madis2@users.noreply.github.com>
2022-04-04 13:23:46 +0300

    Update tekstipohinemang.py

--- tekstipohinemang.py
+++ tekstipohinemang.py
@@ -43,8 +43,6 @@ defense = 0   # See muutuja lisatakse juurde protsendile.
 protsent = 30 # Protsent tõstab bloki õnnestumist.
 level = 0     # Muutuja mitmendal tasemel mängija on.
 
-km = random.randint(50,150) # Lambine muutuja 
-
 # Funktsioon valikud võitluse jaoks.
 
 def valikud():
@@ -72,7 +70,7 @@ def kaklus(peasant_health, peasant_kuld, peasant_attack, xp_attack, xp_defense):
         
         dmg = random.randint(5,10) # Kahju mis teed vastasele
         booster = dmg*attack
-        peasant_dmg = random.randint(1,5) # Kahju mis tehakse sulle
+        peasant_dmg = random.randint(3,10) # Kahju mis tehakse sulle
         peasant_dmg = peasant_dmg
         
         print('─────────────────────────────────────')
@@ -156,14 +154,22 @@ if level == 0:
     print()
     time.sleep(2)
     print('Alustad võitlust talupojaga kanamunade üle. \nMida soovid teha?')
+    print()
+    time.sleep(2)
     
-    kaklus(50,10,1,0.05,5)
+    kaklus(50, random.randint(7,10), 1, 0.05, 1)
     print('Talupoeg on koomas ja sa otsustad jätkata enda seiklust.')
+    print()
+    print('+0.05 ATTACK')
+    print('+1% DEFENSE')
     level += 1
+    print()
+    
+    
 if level == 1:
     print('TASE 2')
     print('─────────────────────────────────────')
-    print(f'Läbisid {km} kilomeetrit')
+    print(f'Läbisid {random.randint(50,150)} kilomeetrit')
     time.sleep(2)
     print('Liikusid edasi läände ja leidsid vanatädi. \nVanatädi soovitas liikuda edasi läände. \nEnda nime ta ei avaldanud.')
     time.sleep(5)
@@ -173,7 +179,7 @@ if level == 1:
     print('Alustad võitlust ühe piraadiga. Teine vaatab pealt.')
     print()
     
-    kaklus(60,15,1,0.1,2.5)
+    kaklus(60, random.randint(10,15), 1.5, 0.03, 2)
     
     print('Piraat Tõnu on teadvusetu.')
     time.sleep(2)
@@ -182,5 +188,71 @@ if level == 1:
     print()
     time.sleep(2)
     
-    kaklus(70,20,1.25,0.2,2.5)
-    kaklus()
+    kaklus(70, random.randint(15,20), 2, 0.04, 2)
+    
+    print('Oled mõlemad piraadid hukanud')
+    time.sleep(2)
+    print('Jätkad oma seiklust läände.')
+    level += 1
+    
+    print('+0.07 ATTACK')
+    print('+4% DEFENSE')
+    print()
+    
+if level == 2:
+    print('TASE 3')
+    print('─────────────────────────────────────')
+    print(f'Läbisid {50,150} kilomeetrit.')
+    time.sleep(2)
+    print('Oled pikalt liikunud läände ja edusamme nähtaval ei ole.')
+    time.sleep(2)
+    print('Teed väikse puhkepausi et süüa ja taastada energia ja tervis.')
+    health += 20
+    time.sleep(5)
+    print()
+    print('Taastasid 20 elu.')
+    print()
+    time.sleep(2)
+    print('Puhkepausi tehes äkiliselt rünnatakse sind seljatagant.')
+    print('Vastane lõi mööda ning valmistud sõdimiseks.')
+    
+    kaklus(85, random.randint(20,25), 3, 0.1, 3)
+    print('Võitsid vastase kuid siiamaani ei saa aru kes või mis see on.')
+    time.sleep(2)
+    print('Sind ei huvita ning jätkad seiklust')
+    print()
+    time.sleep(2)
+    print('+0.1 ATTACK')
+    print('+3% DEFENSE')
+    print()
+    level += 1
+
+if level == 3:
+    print('TASE 4')
+    print('─────────────────────────────────────')
+    print(f'Läbisid {random.randint(50,150)} kilomeetrit.')
+    time.sleep(2)
+    print('Leiad posteri kus tuleb välja, et oled pärast eelnevaid mõrvu \nkuulutatud tagaotsitavaks')
+    time.sleep(3)
+    print('Sinu mõrvamise autasu on {kuld}')
+    time.sleep(2)
+    print('Kõnnid edasi mitte midagi arvates sellest.')
+    time.sleep(2)
+    print('Sulle tuleb vastu rüütel.')
+    print('Räägid temaga juttu ja siis ta avastab, et oled tagaotsitav.')
+    time.sleep(2)
+    print('"Vabandust {user_name} oli tore vestelda aga \nkahjuks ei jää mul muud üle kui tappa sind."')
+    print()
+    time.sleep(3)
+    print('Alustad võitlust rüütliga.')
+    print('Rüütlil on raudrüü seljas kuid oled piisavalt tugev et võita.')
+    
+    kaklus(100, random.randint(20,30), 5, 0.5, 7)
+    
+    time.sleep(2)
+    print('Sul vedas, et võitsid.')
+    time.sleep(2)
+    print('Mõtled endaviisi, et hakkad lähemale jõudma kalevipojale.')
+    print('+0.5 ATTACK')
+    print('+7% DEFENSE')
+    print()
